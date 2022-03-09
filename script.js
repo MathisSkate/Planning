36f420b9d0f184fcc605a630fd88ecaeec4002ad
Mathis <mathisleskateur@gmail.com>
2022-03-09 16:38:05 +0100

    Suppression des console log innutile

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 var planning = $("#planning");
 //var cours = $("<textarea class='cours'></textarea>");
 var heures = $(".heure");
-console.log(heures);
 heures.each(function() {
     var heure = $(this);
     heure.click(function() {
-		console.log(heure);
         planning.append($("<textarea class='cours' style='left: " + heure[0].offsetLeft + "px; top: " + heure[0].offsetTop + "px; height: " + heure[0].scrollHeight + "px; width: " + heure[0].scrollWidth + "px;'></textarea>"));
     })
 });
