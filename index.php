9b686fb4427623e093195db4b82d176be5878cef
Mathis <mathisleskateur@gmail.com>
2022-03-09 14:30:21 +0100

    Change index.html to index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Planning</title>
+</head>
+<body>
+<div id="planning">
+    <?php
+
+    ?>
+</div>
+<div id="action"></div>
+</body>
+</html>
