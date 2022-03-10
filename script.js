97ed88f61884984987ba821ba7de5310fdc7b456
Pierre Lebigre <pierre.marc.lebigre@gmail.com>
2022-03-10 09:14:10 +0100

    Cours agrandissable selon les prérequis

--- script.js
+++ script.js
@@ -6,6 +6,19 @@ heures.each(function() {
     var heure = $(this);
     heure.click(function() {
 		console.log(heure);
-        planning.append($("<textarea class='cours' style='left: " + heure[0].offsetLeft + "px; top: " + heure[0].offsetTop + "px; height: " + heure[0].scrollHeight + "px; width: " + heure[0].scrollWidth + "px;'></textarea>"));
+        planning.append($("<div class='cours'></div>"));
+		var coursCourant = $(".cours").last();
+		coursCourant.css("left", heure[0].offsetLeft);
+		coursCourant.css("top", heure[0].offsetTop);
+		coursCourant.css("height", (heure[0].scrollHeight + 5));
+		coursCourant.css("width", (heure[0].scrollWidth + 5));
+		console.log(((heure[0].scrollHeight + 5) * 10));
+		coursCourant.resizable({
+			grid: (heure[0].scrollHeight + 5),
+			maxWidth: (heure[0].scrollWidth + 5),
+			minWidth: (heure[0].scrollWidth + 5),
+			maxHeight: ((heure[0].scrollHeight + 5) * 10),
+			animate: true
+		});
     })
 });
