522b17502093c1ada3add9c97691338d4b7689a6
Pierre Lebigre <pierre.marc.lebigre@gmail.com>
2022-03-10 11:48:50 +0100

    Déplacement des cours

--- script.js
+++ script.js
@@ -1,24 +1,47 @@
 var planning = $("#planning");
-//var cours = $("<textarea class='cours'></textarea>");
 var heures = $(".heure");
-console.log(heures);
-heures.each(function () {
+heures.each(function() {
     var heure = $(this);
-    heure.click(function () {
-        console.log(heure);
-        planning.append($("<div class='cours'></div>"));
-        var coursCourant = $(".cours").last();
-        coursCourant.css("left", heure[0].offsetLeft);
-        coursCourant.css("top", heure[0].offsetTop);
-        coursCourant.css("height", (heure[0].scrollHeight + 5));
-        coursCourant.css("width", (heure[0].scrollWidth + 5));
-        console.log(((heure[0].scrollHeight + 5) * 10));
-        coursCourant.resizable({
-            grid: (heure[0].scrollHeight + 5),
-            maxWidth: (heure[0].scrollWidth + 5),
-            minWidth: (heure[0].scrollWidth + 5),
-            maxHeight: ((heure[0].scrollHeight + 5) * 10),
-            animate: true
-        });
+    heure.click(function() {
+        planning.append($("<div class='cours'><span class='spanHeure'></span></div>"));
+
+		const coursCourant = $(".cours").last();
+		const spanHeure = $('.spanHeure').last()[0];
+		const heureCourante = (heure[0].innerText.replace(" H", ""));
+		const heureHeight = (heure[0].scrollHeight + 5);
+		const heureWidth = (heure[0].scrollWidth + 5);
+		const heureTop = heure[0].offsetTop - 2.5;
+		const heureLeft = heure[0].offsetLeft - 2.5;
+
+		coursCourant.css("left", heureLeft);
+		coursCourant.css("top", heureTop);
+		coursCourant.css("height", heureHeight);
+		coursCourant.css("width", heureWidth);
+		spanHeure.innerText = heureCourante + " - " + (parseInt(heureCourante) + 1);
+
+		coursCourant.resizable({
+			grid: heureHeight,
+			maxWidth: heureWidth,
+			minWidth: heureWidth,
+			maxHeight: ((heureHeight * 10) - (heureTop - 31)),
+			animate: true,
+			resize : function (e, ui){
+				const originText = ui.originalElement[0].innerText.split(" ");
+				spanHeure.innerText = originText[0] + " " + originText[1] + " " + Math.round(parseInt(originText[0]) + (ui.size.height / heureHeight));
+			}
+		});
+		coursCourant.draggable({
+			axis: "y",
+			scroll: true,
+			grid: [ 0, (heureHeight) ],
+			stop: function (e, ui){
+				const originText = ui.helper[0].innerText.split(" ");
+				const heureDepart = Math.round(parseInt(originText[0]) + ((ui.position.top - ui.originalPosition.top) / (heureHeight + 2.5)));
+				const heureFin = Math.round(heureDepart + ((ui.helper[0].scrollHeight) / (heureHeight)));
+				spanHeure.innerText = heureDepart + " " + originText[1] + " " + heureFin;
+			}
+
+		});
+
     })
 });
