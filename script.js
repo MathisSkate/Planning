65d200d9b02dcb4beeaec8af7bc44e9bc9b0c81f
Pierre Lebigre <pierre.marc.lebigre@gmail.com>
2022-03-11 08:43:04 +0100

    Ajout du prof et de la matière au cours ey Inscription de celui-ci en base

--- script.js
+++ script.js
@@ -47,9 +47,92 @@ heures.each(function() {
 				spanHeure.innerText = heureDepart + " " + originText[1] + " " + heureFin;
 
 				addResizable(coursCourant, ui.helper[0].offsetTop);
+
+				if($(this).find(".profName").length === 1 && $(this).find(".matiereName").length === 1){
+					$.ajax({
+						url: "ucours.php/?cours" + $(this).find(".idCours") + "&debut=" + debut + "&fin=" + fin + "",
+						method: "GET",
+						dataType : "json",
+						success: function (data) {
+						},
+					});
+				}
 			}
 		});
 
+		coursCourant.droppable({
+			drop: function(event, ui){
+				if(ui.draggable.find(".profName").length !== 0 && $(this).find(".profName").length === 0){
+					var p = $('<br/><p class="idProf" hidden>' + ui.draggable.find("p")[0].innerText + '</p><span class="profName">' + ui.draggable.find("span")[0].innerText + '</span>');
+					$(this).append(p);
+				}
+				if(ui.draggable.find(".matiereName").length !== 0 && $(this).find(".matiereName").length === 0){
+					var p = $('<br/><p class="idMatiere" hidden>' + ui.draggable.find("p")[0].innerText + '</p><span class="matiereName">' + ui.draggable.find("span")[0].innerText + '</span>');
+					$(this).append(p);
+				}
+				if($(this).find(".profName").length === 1 && $(this).find(".matiereName").length === 1){
+					console.log($(this).find("span.spanHeure"));
+					const debut = $(this).find("span.spanHeure")[0].innerText.split(" ")[0];
+					const fin = $(this).find("span.spanHeure")[0].innerText.split(" ")[2];
+					$.ajax({
+						url: "cours.php/?prof=" + $(this).find(".idMatiere")[0].innerText + "&matiere=" + $(this).find(".idMatiere")[0].innerText + "&debut=" + debut + "&fin=" + fin + "",
+						method: "GET",
+						dataType : "json",
+						success: function (data) {
+							var spanIdCours = $('<span class="idCours">' + data + '</span>')
+						},
+					});
+				}
+			}
+		})
     })
 });
+
+$.when(initProf().done(), initMatiere().done()).then( () => {
+	$(".prof").each(function() {
+		var prof = $(this);
+		prof.draggable({
+			revert: "invalid",
+			helper: "clone",
+			cursor: "move"
+		});
+	});
+
+	$(".matiere").each(function() {
+		var matiere = $(this);
+		matiere.draggable({
+			revert: "invalid",
+			helper: "clone",
+			cursor: "move"
+		});
+	});
+});
+
+function initProf() {
+	return $.ajax({
+		url: "profs.php",
+		method: "GET",
+		dataType : "json",
+		success: function (data) {
+			data.forEach(prof => {
+				const divProf = $('<div class="prof"><p hidden>' + prof.prof_id + '</p><span class="profName">' + prof.prof_libelle + '</span></div>');
+				$('#profs').append(divProf);
+			});
+		},
+	});
+}
+
+function initMatiere() {
+	return $.ajax({
+		url: "matieres.php",
+		method: "GET",
+		dataType : "json",
+		success: function (data) {
+			data.forEach(matiere => {
+				const divMatiere = $('<div class="matiere"><p hidden>' + matiere.matiere_id + '</p><span class="matiereName">' + matiere.matiere_libelle + '</span></div>');
+				$('#matieres').append(divMatiere);
+			});
+		},
+	});
+}
 
